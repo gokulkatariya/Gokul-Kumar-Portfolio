75269c0a0f9476cb7f0f58e58f7f10f8b0b65db3
gokulkatariya <127433672+gokulkatariya@users.noreply.github.com>
2024-09-14 09:01:15 +0530

    commit

--- src/components/ProjectSec.jsx
+++ src/components/ProjectSec.jsx
@@ -5,6 +5,8 @@ import BookStore from "../assets/image/BookStore.webp";
 import RestApi from "../assets/image/RestApi.webp";
 import Caffe from "../assets/image/Caffe.webp";
 import Expenses from "../assets/image/Expenses.webp";
+import google_gemini from "../assets/image/googlegemini.png";
+import collage_site from "../assets/image/collagesite.png";
 
 function ProjectSec() {
   return (
@@ -13,7 +15,148 @@ function ProjectSec() {
         <h2 className="text-3xl sm:text-[40px] bg-[#111] relative z-10 font-bold px-4 py-2 w-max mx-auto text-center text-[#47c0e8] sm:border-b-2 border-[#1788ae] mt-5">
           Latest Works
         </h2>
-        <div className="flex flex-col sm:flex-row gap-4 mt-8 sm:gap-[80px] items-center sm:mt-20 relative">
+        <div className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative">
+          <div className="h-[1px] left-1/4 right-1/2 bg-[#1788ae] absolute top-1/2 hidden sm:block"></div>
+          <div className="w-4 h-4 rounded-full border-[3px] border-[#e65580] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"></div>
+          <a
+            href="https://gleaming-sundae-df77e0.netlify.app/"
+            target="_blank"
+            className="flex w-full relative justify-center sm:justify-start"
+          >
+            <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200">
+              <img
+                className="max-w-[400px] w-full mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)]"
+                src={google_gemini}
+                alt="googlegemini"
+              />
+              <span className="flex group-hover:-top-14 ease-jump duration-200 bg-[#e65580] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+                Google Gemini
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-external-link"
+                >
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              </span>
+            </div>
+          </a>
+
+          <div className="w-full">
+            <h3 className="text-[#e65580] font-bold text-2xl md:text-4xl">
+              Google Gemini
+            </h3>
+            <span className="text-[#e65580] text-base md:text-lg">
+              (Search Engine)
+            </span>
+            <p className="text-justify text-sm md:text-base mt-2">
+              Being a lead developer, revamped the site to a highly responsive,
+              and interactive website. Created new features and pages. Worked as
+              a team with product manager and ux designer.
+            </p>
+
+            <ul className="flex flex-wrap gap-2 mt-2">
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #react.js
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #api
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #html
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #css
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #javascript
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #figma
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative">
+          <div className="order-2 sm:order-1 w-full">
+            <h3 className="text-[#7896dd] font-bold text-2xl md:text-4xl">
+              Collage Site
+            </h3>
+            <span className="text-[#7896dd] text-base md:text-lg">
+              ( Education For a Better World)
+            </span>
+            <p className="text-justify text-sm md:text-base mt-2">
+              Being a lead developer, revamped the site to a highly responsive,
+              and interactive website. Created new features and pages. Worked as
+              a team with product manager and ux designer.
+            </p>
+
+            <ul className="flex flex-wrap gap-2 mt-2">
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #react.js
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #form submision
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #html
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #css
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #javascript
+              </li>
+              <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base">
+                #figma
+              </li>
+            </ul>
+          </div>
+          <div className="h-[1px] left-1/2 right-1/4 bg-[#1788ae] absolute top-1/2 hidden sm:block"></div>
+          <div className="w-4 h-4 rounded-full border-[3px] border-[#7896dd] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"></div>
+          <a
+            href="https://dazzling-vacherin-1fd7ae.netlify.app/"
+            target="_blank"
+            className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start"
+          >
+            <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200 sm:ml-auto">
+              <img
+                className="max-w-[400px] w-full relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)]"
+                src={collage_site}
+                alt="collage-site"
+              />
+              <span className="flex group-hover:-top-14 ease-jump duration-200 text-black bg-[#7896dd] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+                Collage Site
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-external-link"
+                >
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              </span>
+            </div>
+          </a>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative">
           <div className="h-[1px] left-1/4 right-1/2 bg-[#1788ae] absolute top-1/2 hidden sm:block"></div>
           <div className="w-4 h-4 rounded-full border-[3px] border-[#459bd5] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"></div>
           <a
